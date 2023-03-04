038434669710a806063c838109a99b5361fcc8b4
Jahaira Marrero <jahaira.marrero@gmail.com>
2023-03-04 03:12:04 -0500

    added header

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+from sodapy import Socrata
 import numpy as np
 import pydeck as pdk
 import altair as alt
@@ -9,13 +10,12 @@ import requests
 st.header('SAFEST NYC AREAS')
 
 data = requests.get("https://data.cityofnewyork.us/resource/qgea-i56i.json").json()
+client = Socrata("data.cityofnewyork.us", None)
+
 @st.cache_data
 def load_data(url):
-    df = pd.read_json(data)
-    return df
+    results = client.get()
+    results_df = pd.DataFrame.from_records(results)
+    return results_df
 
-df = load_data("https://data.cityofnewyork.us/resource/qgea-i56i.json")
-st.dataframe(df)
-
-st.button("Rerun")
 
