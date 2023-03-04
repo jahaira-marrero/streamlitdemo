cb6745c35671ad70ebdf2124b531705b3acb0c78
Jahaira Marrero <jahaira.marrero@gmail.com>
2023-03-04 02:56:53 -0500

    added header

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import altair as alt
+import requests
 
-st.set_page_config(layout="wide", page_title="Safest NYC Areas")
 
 st.header('SAFEST NYC AREAS')
 
+data = requests.get("https://data.cityofnewyork.us/resource/qgea-i56i.json").json()
+@st._experimental_singleton
+st.json(data, expanded=True)
 
