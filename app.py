3cc7fb2300821570a0a737af56e42ea97344bdf4
Jahaira Marrero <jahaira.marrero@gmail.com>
2023-03-04 02:13:53 -0500

    added header

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pydeck as pdk
 import altair as alt
 
-st.set_page_config(layout="wide", page_title="Safest NYC Areas", page_icon=":check:")
+st.set_page_config(layout="wide", page_title="Safest NYC Areas")
 
 st.header('SAFEST NYC AREAS')
 
