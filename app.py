01c292c385ccba0ec5b8c8def57825cccc690482
Jahaira Marrero <jahaira.marrero@gmail.com>
2023-03-04 02:09:53 -0500

    added header

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st 
 import pandas as pd
+import numpy as np
+import pydeck as pdk
+import altair as alt
 
-print('test')
+st.set_page_config(layout="wide", page_title="Safest NYC Areas", page_icon=":check:")
+
+
